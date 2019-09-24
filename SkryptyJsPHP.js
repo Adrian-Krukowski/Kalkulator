7a167cd6fcaa89ab04ad9fd23ad6535c5a5019c3
Adrian-Krukowski <37860918+Adrian-Krukowski@users.noreply.github.com>
2019-09-24 15:52:14 +0200

    char clear
    -Usuwanie ostanitego znaku z lancucha
    -Klawisz czyszczacy

--- SkryptyJsPHP.js
+++ SkryptyJsPHP.js
@@ -1,22 +1,46 @@
-var exp = '', sol = '';
+var exp = '', sol = '',expA = '',expB = '';
 
 function czyszczenie(){
-    document.Licznik.LicznikTekst.value ="";
+    document.Licznik.liczbaA.value ="";
+    document.Licznik.liczbaB.value ="";
+    document.Licznik.licznikOperator.value ="";
 
 }
 
 function usuniecieOstatniegoZnaku(){
-    exp = document.Licznik.LicznikTekst.value;
-    document.Licznik.LicznikTekst.value = exp.substring(0,exp.length-1);
+    expA= document.Licznik.liczbaA.value
+    expB= document.Licznik.liczbaB.value
+    exp= document.Licznik.licznikOperator.value;
+    if(expB == "" && exp == ""){
+
+        document.Licznik.liczbaA.value = expA.substring(0,expA.length-1);
+    }
+    else if(expB == "" && exp != ""){
+
+        document.Licznik.licznikOperator.value = exp.substring(0,exp.length-1);
+    }
+    else if(expB != ""){
+
+        document.Licznik.liczbaB.value = expB.substring(0,expB.length-1);
+    }
 }
 
 function insert(liczba) {
         if(document.Licznik.licznikOperator.value == ''){
     document.Licznik.liczbaA.value = document.Licznik.liczbaA.value+liczba
     }
-        if(document.Licznik.licznikOperator.value != '' &&  document.Licznik.liczbaA.value !='')
+        if(document.Licznik.licznikOperator.value != '')
         {
         document.Licznik.liczbaB.value = document.Licznik.liczbaB.value+liczba
+
+    }
+}
+
+function jedenOperator(op) {
+
+    if(document.Licznik.liczbaA.value != ''){
+        document.Licznik.licznikOperator.value = exp + op;
+        document.Licznik.licznikOperatorUkryty.value = exp + op;
     }
 }
 
@@ -35,13 +59,7 @@ function obliczaj(){
 
 } */
 
-function jedenOperator(op,) {
 
-        if(document.Licznik.liczbaA != ''){
-        document.Licznik.licznikOperator.value = exp + op;
-        document.Licznik.licznikOperatorUkryty.value = exp + op;
-    }
-}
 
     //     var operator = document.getElementsByClassName("ButtonDzialan");
     //     exp = document.Licznik.LicznikTekst.value;
